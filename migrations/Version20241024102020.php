5b2889c0081b627506a274b68b3f31d195b73e31
AmaimiaGhassan <ghassen_amaimia@issatgf.u-gafsa.tn>
2024-11-28 13:17:32 +0100

    Initial commit

---
+++ migrations/Version20241024102020.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241024102020 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, url VARCHAR(255) NOT NULL, alt VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE plat (id INT AUTO_INCREMENT NOT NULL, regime_id INT DEFAULT NULL, image_id INT DEFAULT NULL, nomplat VARCHAR(255) NOT NULL, cout NUMERIC(10, 0) NOT NULL, nbre_calories INT NOT NULL, ingredients VARCHAR(255) NOT NULL, INDEX IDX_2038A20735E7D534 (regime_id), INDEX IDX_2038A2073DA5256D (image_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE regime (id INT AUTO_INCREMENT NOT NULL, nomregime VARCHAR(255) NOT NULL, duree INT NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE plat ADD CONSTRAINT FK_2038A20735E7D534 FOREIGN KEY (regime_id) REFERENCES regime (id)');
+        $this->addSql('ALTER TABLE plat ADD CONSTRAINT FK_2038A2073DA5256D FOREIGN KEY (image_id) REFERENCES image (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE plat DROP FOREIGN KEY FK_2038A20735E7D534');
+        $this->addSql('ALTER TABLE plat DROP FOREIGN KEY FK_2038A2073DA5256D');
+        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE plat');
+        $this->addSql('DROP TABLE regime');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
